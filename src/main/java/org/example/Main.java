b2ff73991b481ee23a34b2635fb10c40651c551e
laju <lopamudrasahoo.laju@gmail.com>
2025-07-12 00:06:53 +0530

    add new selenium

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,37 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+import java.util.Set;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Set the path to your ChromeDriver executable if not in PATH
-        // System.setProperty("webdriver.chrome.driver", "path/to/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get("https://www.flipkart.com");
-        System.out.println("Navigated to Flipkart");
-        driver.quit();
-
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        WebElement search = driver.findElement(By.name("q"));
+        search.sendKeys("iphone 15");
+        search.submit();
+        String ID=driver.getWindowHandle();
+        System.out.println("ID ="+ID);
+        WebElement iphone = driver.findElement(By.xpath("//img[@class='DByuf4'and@alt='Apple iPhone 15 (Black, 128 GB)']"));
+        iphone.click();
+        Set<String>childID=driver.getWindowHandles();
+        childID.remove(ID);
+        for(String iD:childID){
+            if(iD.equals(ID)) {
+                driver.switchTo().window(iD);
+                break;
+            }
+            }
+        WebElement ADD=driver.findElement(By.xpath("//button[text()='Add to cart']"));
+        ADD.click();
+        System.out.println("Navigated to flipkart add to cart a product");
+//       driver.quit();
     }
 }
+
